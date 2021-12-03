34c606431e670541b7e0b6c9a5d555d4ef7bfc66
X3eRo0 <psinghania256@gmail.com>
2021-12-03 20:51:13 +0530

    asd

---
+++ i3/lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+icon="$HOME/Pictures/lock.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -blur 0x2.5 -resize 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
+rm "$tmpbg"
